fff5ee48bcd52dc77152bede3374b335d0c363a7
Александр <sanek.miron2@gmail.com>
2026-08-23 15:58:31 +0300

    update

--- app/server/actionMtsLink.ts
+++
@@ -1,5 +0,0 @@
-import sql from '../services/lib/db'
-
-export async function dbSaveMtsLink(orderId: number, link: string) {
-    await sql`UPDATE orders SET link = ${link} WHERE id = ${orderId}`
-}
