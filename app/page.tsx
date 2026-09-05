2a1f15dde52a830e61f61633bd5dfc880a46c62b
Александр <sanek.miron2@gmail.com>
2026-09-06 00:00:14 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 // import Image from 'next/image'
-import styles from './styles/Home.module.scss'
+// import styles from './styles/Home.css'
+import './styles/Home.scss'
 import Wrapper from '@/app/components/wrapper/wrapper'
 export default function Home() {
     return (
         <div>
-            <main className={styles.main}>
+            <main className="main">
                 <Wrapper />
             </main>
         </div>
